39b0b85165683891c84b8537692fb94bc075a043
jftucker <johntucker0686@gmail.com>
2024-03-13 15:57:57 -0600

    handle resource does not exist with 404

--- api/index.js
+++ api/index.js
@@ -17,5 +17,18 @@ app.get('/api/fetch/', async (req, res) => {
   }
 });
 
+app.get('/api/fetch/:id', async (req, res) => {
+  const { id } = req.params;
+  const query = 'SELECT * FROM users WHERE ID = $1';
+  try {
+    const result = await db.query(query, [id]);
+    if (!result.rows[0]) return res.status(404).send('Resource does not exist');
+    return res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
