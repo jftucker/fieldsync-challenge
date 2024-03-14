0dd967558cb55580d6be68a1b0b04334f866c879
jftucker <johntucker0686@gmail.com>
2024-03-13 20:07:02 -0600

    refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@chakra-ui/react';
 import UserTable from './components/UserTable';
 import usePlaceholderUsers from './hooks/usePlaceholderUsers';
 
--- frontend/src/components/UserTable.tsx
+++ frontend/src/components/UserTable.tsx
@@ -1,6 +1,5 @@
 import {
   Table,
-  TableCaption,
   TableContainer,
   Tbody,
   Td,
