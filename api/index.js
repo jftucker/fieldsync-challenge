ce04b2e01b41ede4071851fe0391a2509d3fcaa6
jftucker <johntucker0686@gmail.com>
2024-03-14 03:38:21 -0600

    remove unused endpoint

--- api/index.js
+++ api/index.js
@@ -7,10 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
 app.get('/api/fetch/', async (req, res) => {
   try {
     const result = await db.query('SELECT * FROM users');
